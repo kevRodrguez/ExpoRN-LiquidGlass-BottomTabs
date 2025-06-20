01ba5c602c9fa109453fdda01af4abe56d3a45b3
Kevin Rodriguez <kevin.rodriguez5@catolica.edu.sv>
2025-06-20 11:12:13 -0600

    Liquid Glass Bottom tab Expo React Native implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
 
+
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
